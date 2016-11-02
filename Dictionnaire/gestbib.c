377eb09892723d60c484dde1dfe8bb2b0f2a1f37
mickafon <micaelafonso@free.fr>
2016-11-02 20:17:03 +0100

    Ajout fonctions mickael

--- Dictionnaire/gestbib.c
+++ Dictionnaire/gestbib.c
@@ -254,3 +254,64 @@ LinkedList* split_str(LinkedList* liste, char* str, char* delimiter)
     return tmp;
 }
 
+int checkWord(Dictionary* _Dictionary, char* word){
+    FILE* f = fopen(_Dictionary->filepath, "r+");
+    int i = 0;
+    char carac;
+    if(f != NULL){
+        while((carac = fgetc(f)) != EOF){
+            if(word[i] == carac){
+                i++;
+                if(word[i] == '\0' && fgetc(f) == '\n'){
+                    return -1;
+                }
+            } else{
+                i = 0;
+            }
+        }
+        fclose(f);
+    } else{
+        printf("Impossiblie d'ouvrir le fichier %s", _Dictionary->filepath);
+    }
+    return 0;
+}
+
+//Afficher
+void launcher(){
+    int choix = 0;
+    choix = menu();
+
+    switch(choix){
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    }
+}
+int menu(){
+    int choix = 0;
+    printf("APPLICATION DE GESTION D'UN DICTIONNAIRE \n");
+    printf("Menu : \n");
+    printf("1 - Créer un fichier dictionnaire \n");
+    printf("2 - Utiliser un dictionnaire existant \n");
+    printf("3 - Créer un dictionnaire à partir d'un fichier texte \n");
+    printf("4 - Supprimer un fichier dictionnaire \n");
+    printf("5 - Ajouter un mot a un fichier dictionnaire \n");
+    printf("6 - Rechercher un mot dans un fichier dictionnaire \n\n");
+
+    while(choix < 1 || choix > 6){
+        printf("Selectionner un choix : ");
+        scanf("%d", &choix);
+    }
+    system("cls");
+    return choix;
+}
+
